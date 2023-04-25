375acdb87078aa41e085f13723dfadd14d0c8f39
phunm7479 <phunm7479@cowell.vn>
2023-04-25 16:26:40 +0700

    train part 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Trainning from './component/Hook/Trainning';
 import {
   BrowserRouter as Router,
   Switch,
